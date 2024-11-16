ae122dab8c01b929c2e62f6a52d85d53f26f70f4
Eric Nichols <underspecified@gmail.com>
2024-11-16 23:06:57 +0900

    merge

--- setup.sh
+++ setup.sh
@@ -19,6 +19,5 @@ for f in *; do
     echo_and_eval "ln -sf \"$PWD/$f\" \"$HOME/.config/$f\""
 done)
 
-echo_and_eval "(cd dot.ssh && chmod go-rwX github github_rsa gmail hri_jp)"
+echo_and_eval "(cd dot.ssh && chmod go-rwX github github hri_jp)"
 echo_and_eval "ln -sf $HOME/.zsh/dot.zshrc $HOME/.zshrc"
-echo_and_eval "ln -sf $HOME/.zsh/dot.zshrc $HOME/.profile"
