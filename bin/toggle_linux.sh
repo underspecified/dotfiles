d6f9e88979f031cff1a900e21444d52acdabf6e5
Eric Nichols <underspecified@gmail.com>
2025-01-14 18:35:48 +0900

    toggle wm theme, firefox, zed

--- bin/toggle_linux.sh
+++ bin/toggle_linux.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 get_gnome_mode () {
-    gsettings get org.gnome.desktop.interface gtk-theme |
-    grep -i light && echo "light" || echo "dark"
+    (gsettings get org.gnome.desktop.interface gtk-theme |
+     grep -i dark) >/dev/null && \
+    echo "dark" || echo "light"
 }
 
 toggle_gnome_dark () {
@@ -15,6 +16,20 @@ toggle_gnome_light () {
     gsettings get org.gnome.desktop.interface gtk-theme |
     sed 's/dark/light/; s/Dark/Light/' |
     xargs gsettings set org.gnome.desktop.interface gtk-theme
+}
+
+focus_window () {
+    win_id=$(xwininfo -root -tree |
+             grep "$@" |
+             awk '{print $1}')
+    xdotool windowactivate $win_id
+    xdotool windowfocus $win_id
+}
+
+toggle_firefox () {
+    focus_window '("Navigator" "firefox")'
+    xdotool key "alt+shift+d"
+    focus_window '("kitty" "kitty")'
 }
 
 toggle_gnome () {
@@ -32,21 +47,31 @@ toggle_kitten () {
     kitten themes --config-file-name=themes.conf --reload-in=all "$theme"
 }
 
+toggle_regolith () {
+    [[ "$1" == "dark" ]] && theme="selenized-dark" || theme="selenized-light"
+    regolith-look set "$theme" &>/dev/null
+}
+
 toggle_zed () {
     sed -i.bak -r 's/"mode": ".+"/"mode": "'$1'"/' \
         ~/git/dotfiles/config/zed/settings.json
 }
 
+curr=$(get_gnome_mode)
 if [[ $1 == "dark" ]]; then
     mode="dark"
 elif [[ $1 == "light" ]]; then
     mode="light"
 else
-    [[ $(get_gnome_mode) == "light" ]] && mode="dark" || mode="light"
+    [[ $curr == "light" ]] && mode="dark" || mode="light"
 fi
-# echo "mode: $mode"
+echo "$curr => $mode"
 
-toggle_gnome $mode
-toggle_i3 $mode
-toggle_kitten $mode
-toggle_zed $mode
+if [[ $curr != $mode ]]; then
+    #toggle_firefox $mode
+    toggle_gnome $mode
+    #toggle_i3 $mode
+    toggle_regolith $mode
+    toggle_kitten $mode
+    toggle_zed $mode
+fi
