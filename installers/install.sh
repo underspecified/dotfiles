3c9b873f5a684c5f2842799e0e416774f4db3b0a
Eric Nichols <underspecified@gmail.com>
2025-04-08 15:07:45 +0900

    fixed i3lock install

--- installers/install.sh
+++ installers/install.sh
@@ -91,9 +91,10 @@ install_i3lock () {
     sudo apt remove i3lock
 
     # install build deps
-    sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
+    sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libgif-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
 
-    (git clone https://github.com/Raymo111/i3lock-color.git
+    (cd ~/git
+     git clone https://github.com/Raymo111/i3lock-color.git
      cd i3lock-color
      sudo ./install-i3lock-color.sh)
 )
