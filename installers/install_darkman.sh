fa1cef444f1502b76d84a5b21e4d4084f3923331
Eric Nichols <underspecified@gmail.com>
2025-05-21 14:19:37 +0900

    darkman: refactored service settings and added installer

---
+++ installers/install_darkman.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+CUR_DIR=$(pwd)
+GIT_DIR=$(realpath "$CUR_DIR/../..")
+
+install_darkman() {
+    echo "Installing darkman..."
+
+    # clone repo
+    cd "$GIT_DIR"
+    git clone https://gitlab.com/WhyNotHugo/darkman.git
+
+    # build program
+    cd darkman
+    make
+
+    # install program
+    sudo make install PREFIX=/usr
+
+    # setup service
+    systemctl --user enable darkman.service
+    mkdir -p "$HOME/.config/systemd/user/darkman.service.d/"
+    cp "$GIT_DIR/dotfiles/config/darkman/override.conf" "$HOME/.config/systemd/user/darkman.service.d/override.conf"
+
+    # start service
+    systemctl --user daemon-reload
+}
+
+install_darkman
