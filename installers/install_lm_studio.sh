dba4aa4a45247bbe2619e893e23365246d9a9192
Eric Nichols <underspecified@gmail.com>
2025-06-05 10:21:26 +0900

    added lm-studio installer

---
+++ installers/install_lm_studio.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+install_lm_studio () {
+    curl https://installers.lmstudio.ai/linux/x64/0.3.16-8/LM-Studio-0.3.16-8-x64.AppImage \
+        --create-dirs \
+        --output-dir ~/.local/lm-studio.app \
+        --output LM-Studio-0.3.16-8-x64.AppImage
+
+        cd ~/.local/lm-studio.app
+    chmod +x LM-Studio-0.3.16-8-x64.AppImage
+    ln -sf LM-Studio-0.3.16-8-x64.AppImage LM-Studio-0.3.16-8-latest.AppImage
+
+    cat << 'EOF' > ~/.local/lm-studio.app/lm-studio
+#!/bin/bash
+
+$HOME/.local/lm-studio.app/LM-Studio-latest-x64.AppImage --no-sandbox "$@"
+EOF
+
+    chmod +x ~/.local/lm-studio.app/lm-studio
+    ln -sf ~/.local/lm-studio.app/lm-studio ~/.local/bin/
+}
+
+install_lm_studio
